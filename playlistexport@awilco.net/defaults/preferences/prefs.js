96fda192e28a8a3bb6ecbfe6d4c1e6064362b0f3
John Murray <johnmurrayvi@gmail.com>
2012-04-14 19:00:53 -0400

    initial commit of ported playlist export extension for nightingale. it works but still has a few bugs

---
+++ playlistexport@awilco.net/defaults/preferences/prefs.js
@@ -0,0 +1,18 @@
+pref("addons.playlistexport.exportLocation","");
+pref("addons.playlistexport.exportFormat",1);
+//AW: preference for relative paths
+pref("addons.playlistexport.relativePaths",true);
+//AW: preference forusing backslash
+pref("addons.playlistexport.useBackslash",false);
+//AW: preference for using correct case alogrithm
+pref("addons.playlistexport.correctCase",true);
+// the line below is used to localize the initial description
+pref("extensions.playlistexport@hectoravila.com.description", "chrome://playlistexport/locale/description.properties");
+
+// The below settings do not exist yet until release of
+// Playlist Export Tool 0.2 Beta 1 because they
+// have not been implemented yet
+
+//pref("addons.playlistexport.options.filterOutUnnamedPlaylists",true);
+//pref("addons.playlistexport.options.filterOutHiddenPlaylistItems",true);
+//pref("addons.playlistexport.options.filterOutEmptyPlaylists",true);
