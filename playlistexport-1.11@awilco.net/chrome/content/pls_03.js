949f80e16e9b0f866c3aacf6b359e1749c0560ad
John Murray <johnmurrayvi@gmail.com>
2012-04-14 21:12:12 -0400

    this one works with 1.11+

---
+++ playlistexport-1.11@awilco.net/chrome/content/pls_03.js
@@ -0,0 +1,5 @@
+var pls_export={
+	exportPlaylist:function(){
+		window.openDialog("chrome://playlistexport/content/export_dialog03.xul","export_dialog","dialog=yes,modal=yes,alwaysLowered=no,centerscreen=yes,resizable=no");
+	}
+};
