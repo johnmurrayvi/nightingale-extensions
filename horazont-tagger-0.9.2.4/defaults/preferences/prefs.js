82e313a09897e15323def0b6e2fb597e04f06d64
John Murray <johnmurrayvi@gmail.com>
2012-04-15 22:21:26 -0400

    initial commit of working ported tagger extension

---
+++ horazont-tagger-0.9.2.4/defaults/preferences/prefs.js
@@ -0,0 +1,13 @@
+pref("extensions.tagger.rename.keepextension", true);
+pref("extensions.tagger.rename.keepcopy", false);
+pref("extensions.tagger.rename.pattern", "%a/%??b/%a - %??n %t");
+pref("extensions.tagger.retag.autoupcase", false);
+pref("extensions.tagger.retag.keeptags", true);
+pref("extensions.tagger.retag.lastfmt", "");
+pref("extensions.tagger.retag.replace_", true);
+pref("extensions.tagger.retag.dirsupport", false);
+pref("extensions.tagger.settags.keeptags", true);
+pref("extensions.tagger.firstrun", true);
+pref("extensions.tagger.filterfilenames", true);
+pref("extensions.tagger.filter.replace", true);
+pref("extensions.tagger.filter.replacement", " ");
