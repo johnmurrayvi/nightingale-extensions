e54ddc4fa9df6b7376affd912b8a2b441757b77e
John Murray <johnmurrayvi@gmail.com>
2013-01-25 10:35:35 -0500

    update build-ext.sh to grab the version name from the install.rdf file so the directories don't need versioned names

--- build-ext.sh
+++ build-ext.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-ext=$1
+ext=`echo $1 | sed 's/\///g'`
+ver=$(cat $ext/install.rdf | grep em:version | sed 's/^[ \t]*//g' | sed 's/[<*>,em:version,\/]//g') 
 bdir=xpis
 if [ ! -d $bdir ] ; then
 	mkdir xpis
-elif [ -f "$bdir/$ext.xpi" ] ; then
+elif [ -f "$bdir/$ext-$ver.xpi" ] ; then
 	rm -f $ext.xpi
 fi
 
 cd $ext
-zip -r ../$bdir/$ext.xpi ./
+zip -r ../$bdir/$ext-$ver.xpi ./
 
